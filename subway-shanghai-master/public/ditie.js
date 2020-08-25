9a4c842460015ef2f7c3fb788893a9ce7e18bdd5
llx <463264977@qq.com>
2020-08-25 11:26:10 +0800

    动态变化

--- subway-shanghai-master/public/ditie.js
+++ subway-shanghai-master/public/ditie.js
@@ -10,8 +10,35 @@ function createAllcirlce() {
             .attr("class", 't')
             .attr("cx", s.attr("cx"))
             .attr("cy", s.attr("cy"))
-            .attr("r", 10)
+            .attr("r", 5)
             .attr("fill", s.attr("stroke"))
             .style('opacity', 0.7);
     });
+
+    g2.selectAll("image").each(function (d, i) {
+        var s = d3.select(this);
+        var cx = String(Number(s.attr("x")) + 8);
+        var cy = String(Number(s.attr("y")) + 8);
+        g2.append("circle").attr("sid", s.attr("dataid"))
+            .attr("statid", s.attr("statid"))
+            .attr("class", 't')
+            .attr("cx", cx)
+            .attr("cy", cy)
+            .attr("r", 5)
+            .attr("fill", "#a1afc9")
+            .style('opacity', 0.7);
+        console.log(cx);
+    });
+}
+
+
+function run() {
+    $.ajax({
+        url: "0401.json",
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            console.log(data)
+        },
+    })
 }
